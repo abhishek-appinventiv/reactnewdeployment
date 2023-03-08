4925e2864860c28ec89b8890c726e0064c3eeb92
Abhishek <abhishek@appinventiv.com>
2023-03-09 02:17:14 +0530

    uploaded build file

---
+++ client/build/precache-manifest.8032a419eaa07ce18d902ec475133a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e3cf6d9eeb1dbec0dd6",
+    "url": "/static/css/main.eae51b52.chunk.css"
+  },
+  {
+    "revision": "2e3cf6d9eeb1dbec0dd6",
+    "url": "/static/js/main.2e3cf6d9.chunk.js"
+  },
+  {
+    "revision": "ac1b352757bf509a61a1",
+    "url": "/static/js/1.ac1b3527.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7492aba86f55c8d3a48597c73f84ed09",
+    "url": "/index.html"
+  }
+];
